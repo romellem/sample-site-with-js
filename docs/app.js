68142571d68182775d75260761ab274da11b8d4e
romellem <romellem@users.noreply.github.com>
2017-07-10 16:01:04 -0500

    Create app.js

---
+++ docs/app.js
@@ -0,0 +1,16 @@
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function() {
+    var p = document.createElement('p');
+    p.innerText = 'I was dynamically added!';
+    p.styles = 'color: red;';
+    
+    // Insert new paragraph into body
+    document.body.appendChild(p);
+});
